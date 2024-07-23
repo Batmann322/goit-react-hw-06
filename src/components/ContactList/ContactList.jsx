a22b5b182c0bf3fe84dec0af1c6baefcd3ffdc38
Nazar <n.kopilchak@gmail.com>
2024-07-23 21:30:28 +0300

    done

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,13 +3,19 @@ import Contact from "../Contact/Contact";
 import { useSelector } from "react-redux";
 import { selectContacts } from "../../redux/contactsSlice";
 import css from "./ContactList.module.css";
+import { selectNameFilter } from "../../redux/filtersSlice";
 
 export default function ContactList() {
   const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <ul className={css.contact}>
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
         <Contact key={contact.id} contact={contact} />
       ))}
     </ul>
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -6,6 +6,9 @@ import css from "./SearchBox.module.css";
 export default function SearchBox() {
   const dispatch = useDispatch();
   const filter = useSelector(selectNameFilter);
+  const handleChange = (e) => {
+    dispatch(changeFilter(e.target.value));
+  };
 
   return (
     <div className={css.container}>
@@ -14,7 +17,7 @@ export default function SearchBox() {
         className={css.input}
         type="text"
         value={filter}
-        onChange={(e) => dispatch(changeFilter(e.target.value))}
+        onChange={handleChange}
         placeholder="Search contacts"
       />
     </div>
